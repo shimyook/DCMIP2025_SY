77447968fc0b159f6556f0f1c2246759d2688a20
shimyook <84070050+shimyook@users.noreply.github.com>
2025-06-03 09:09:55 -0600

    Add files via upload

---
+++ other_scripts/run_regrid_all.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#hor_res=mpasa60
+#out_res=0.5x0.5
+
+hor_res=mpasa120
+out_res=1x1
+expname=sy_dcmip2025_horiz_mount_flow_${hor_res}_cntrl
+input_dir="/glade/work/simchany/dcmip25/${expname}/run"
+
+# Loop through each relevant file in the directory
+for filepath in ${input_dir}/${expname}.cam.h0i.*.nc; do
+  # Extract the date part from the filename
+  filename=$(basename "$filepath") # Get just the filename, not the full path
+  datename=$(echo "$filename" | sed -n 's/.*\.h0i\.\(.*\)\.nc/\1/p')
+
+  filename_no_ext="${filepath%.nc}"
+  # Run the regrid.sh script
+  bash regrid.sh ${hor_res} ${out_res} "${filename_no_ext}"
+done
+
+
+
+
